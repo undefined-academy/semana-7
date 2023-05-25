0681426b04d91295560f9db589d38309d29b170a
Pancratzia <le.ortegaperez@gmail.com>
2023-05-25 16:20:53 -0400

    Header y Footer terminados

---
+++ nextjs/pancratzia-4189/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 border-top bg-dark">
+      <div className="col-12 d-flex align-items-center justify-content-center">
+       
+        <span className="mb-0 text-body-secondary text-white">
+          © 2023 PancraDogs, Inc
+        </span>
+      </div>
+
+    </footer>
+  );
+}
