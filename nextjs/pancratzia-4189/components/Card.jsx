8b80236ca67f7d32078401740f685efd59621729
Pancratzia <le.ortegaperez@gmail.com>
2023-05-26 16:57:39 -0400

    Se termina la página, agregando estilos a las cards para que se vean mejor

--- nextjs/pancratzia-4189/components/Card.jsx
+++ nextjs/pancratzia-4189/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "../styles/Card.module.css";
 
 export default function Card({ title, image, idx }) {
   return (
     <div className="col">
-      <div className="card h-100">
-        <div className="img_container">
+      <div className="card h-100 bg-dark">
+        <div className={styles.imgContainer} >
           <img className="card-img-top" src={image} alt="Card image cap" />
         </div>
         <div className="card-body">
-          <h5 className="card-title text-uppercase text-center">{title}</h5>
+          <h5 className="card-title text-uppercase text-center text-light">{title}</h5>
         </div>
       </div>
     </div>
