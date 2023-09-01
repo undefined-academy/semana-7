f16f98903cf7b143e1efdf96a27542ba7e239f1f
Daniel Restrepo <danielcgrafico@email.com>
2023-09-01 07:24:49 +0200

    Created api to return 10 dogs breed with your picture

---
+++ nextjs/dfrestrepo-1489/pages/api/dogs.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  // Get all lisr dogs breads
+  const dogsList = await fetch("https://dog.ceo/api/breeds/list/all").then(
+    (data) => data.json()
+  );
+
+  // Transform in a array of 10 elemets
+  const onlyBreeds = Object.keys(dogsList.message).slice(0, 10);
+
+  // Map breads and get a rtandom image for create the new object
+  const mapDogs = await Promise.all(
+    onlyBreeds.map(async (dog) => {
+      const dogImages = await fetch(
+        `https://dog.ceo/api/breed/${dog}/images/random`
+      ).then((data) => data.json());
+
+      return {
+        name: dog,
+        image: `${dogImages.message}`
+      };
+    })
+  );
+
+  res.status(200).json(mapDogs);
+}
